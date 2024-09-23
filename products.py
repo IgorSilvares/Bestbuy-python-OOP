b055fc55615b14eb06319dbd0ac129b91941fe34
IgorSilvares <104534919+IgorSilvares@users.noreply.github.com>
2024-09-23 17:59:05 +0200

    update products logic

--- products.py
+++ products.py
@@ -1,25 +1,20 @@
-class Product():
+class Product:
     def __init__(self, name, price, quantity):
-        """
-        Initialize a product.
-        
-        :param name(str): The name of the product.
-        :param price(float): The price of the product.
-        :param quantity(int): The quantity of the product.
-        """
         if name is None:
             raise ValueError("Name should not be empty")
-        if price < 0:
-            raise ValueError("Price should not be less than 0")
-        if quantity < 0:
-            raise ValueError("Quantity should not be less than 0")
+        if name == "":
+            raise ValueError("Name should not be empty")
+        if price <= 0:
+            raise ValueError("Price should be greater than 0")
+        if quantity <= 0:
+            raise ValueError("Quantity should be greater than 0")
         
         self.name = name
-        self.price = prices
+        self.price = price
         self.quantity = quantity
         self.active = True
 
-    def get_quantity(self, quantity):
+    def get_quantity(self):
         return self.quantity
 
     
@@ -54,20 +49,5 @@ class Product():
         return quantity * self.price
 
 
-def main():
-    bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
-    mac = Product("MacBook Air M2", price=1450, quantity=100)
-
-    print(bose.buy(50))
-    print(mac.buy(100))
-    print(mac.is_active())
-
-    print(bose.show())
-    print(mac.show())
-
-    bose.set_quantity(1000)
-    print(bose.show())
 
 
-if __name__ == "__main__":
-    main()
